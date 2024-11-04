5551f8ad986f68476f30dc79346f712a494dedd5
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:25:19 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "INNOVACIONES EN CONTENEDORES PARA TRANSPORTE MARÍTIMO 2024",
+    "autor" => "logistica@uns",
+    "resumen" => "Se presentan nuevas tecnologías en contenedores que mejoran la eficiencia y seguridad en el transporte marítimo.",
 ];
 ?>

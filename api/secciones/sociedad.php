8f882beeff56c9fdca7a8dccdc74c68538bcd371
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:32:19 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "CRECEN LAS INICIATIVAS COMUNITARIAS PARA APOYAR A LAS FAMILIAS VULNERABLES",
+    "autor" => "sociedad@uns",
+    "resumen" => "Organizaciones locales lanzan programas para proporcionar alimentos y recursos a familias en situación de riesgo.",
 ];
 ?>

d816b0fb126c5d82528ef550e4b210b8526c8187
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:28:45 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "CUMBRE MUNDIAL SOBRE CAMBIO CLIMÁTICO REÚNE A LÍDERES GLOBALES EN 2024",
+    "autor" => "internacional@uns",
+    "resumen" => "Líderes de todo el mundo se unen para discutir estrategias y acciones urgentes frente al cambio climático.",
 ];
 ?>

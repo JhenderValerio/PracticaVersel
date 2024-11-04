29902d671880168bd5ec73dc0510591b9ff795ee
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:37:45 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -3,7 +3,7 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$ciencias = [
+$ciencia = [
     "titulo" => "DESCUBRIMIENTO DE UNA NUEVA ESPECIE MARINA EN EL PACÍFICO",
     "autor" => "ciencias@uns",
     "resumen" => "Investigadores han encontrado una nueva especie de pez que podría ofrecer claves sobre la biodiversidad oceánica.",

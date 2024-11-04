b1ac6f576ee9e83c032123c93973fd01e4efc0fb
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:29:26 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "REFORMA EDUCATIVA APROBADA POR EL CONGRESO EN 2024",
+    "autor" => "nacional@uns",
+    "resumen" => "El Congreso ha aprobado una reforma educativa que busca mejorar la calidad de la enseñanza en el país.",
 ];
 ?>

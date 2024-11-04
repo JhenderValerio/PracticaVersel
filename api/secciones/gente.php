6bf0b9ceac99237264f418494224181a1a409a27
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:27:45 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "UN JOVEN INNOVADOR RECIBE RECONOCIMIENTO NACIONAL EN TECNOLOGÍA",
+    "autor" => "gente@uns",
+    "resumen" => "Un estudiante de ingeniería ha sido premiado por su proyecto innovador que busca mejorar la sostenibilidad en el transporte.",
 ];
 ?>

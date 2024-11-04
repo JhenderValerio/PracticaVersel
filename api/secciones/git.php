235b275b60609a14d4d69eff9a02a4ed31d337b2
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:28:11 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "GIT LANZA NUEVAS FUNCIONES PARA MEJORAR LA COLABORACIÓN EN PROYECTOS",
+    "autor" => "tecnologia@uns",
+    "resumen" => "La última actualización de Git introduce herramientas que facilitan la gestión de ramas y la revisión de código en equipo.",
 ];
 ?>

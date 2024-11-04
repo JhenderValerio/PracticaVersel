b055848074210788b09d037c2fa9dfa55a6b9364
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:33:17 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "NUEVA SERIE DE TELEVISIÓN ROMPE RÉCORDS DE AUDIENCIA EN SU ESTRENO",
+    "autor" => "television@uns",
+    "resumen" => "La serie ha cautivado a millones de espectadores con su innovadora trama y personajes complejos, convirtiéndose en un fenómeno cultural.",
 ];
 ?>

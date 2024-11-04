84aad4462096977df57dba7306e19fecb26d36ce
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:22:40 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "FESTIVAL INTERNACIONAL DE ARTES ESCÉNICAS 2024",
+    "autor" => "cultura@uns",
+    "resumen" => "El Festival Internacional de Artes Escénicas regresa con una variedad de espectáculos de todo el mundo.",
 ];
 ?>

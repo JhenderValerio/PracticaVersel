5a4033fe195d31491b7d1c47daa3bb58fb55caf0
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:25:51 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "CRECIMIENTO ECONÓMICO DEL PAÍS ALCANZA UN 5% EN 2024",
+    "autor" => "economia@uns",
+    "resumen" => "Las proyecciones indican que el crecimiento sostenido de las exportaciones impulsa la economía nacional.",
 ];
 ?>

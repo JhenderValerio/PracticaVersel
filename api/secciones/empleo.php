b8d2773879ea7c9dd6b65a0de4eaf6532f85facb
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:26:17 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "AUMENTA LA DEMANDA DE PROFESIONALES EN TECNOLOGÍA EN 2024",
+    "autor" => "empleo@uns",
+    "resumen" => "El sector tecnológico busca cubrir miles de vacantes debido al crecimiento de la digitalización en las empresas.",
 ];
 ?>

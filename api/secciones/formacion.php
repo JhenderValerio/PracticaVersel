0ad943bc7b6ad9c2a3d97fbb459f87f08be45f7b
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:27:05 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "LANZAMIENTO DE NUEVAS CERTIFICACIONES EN HABILIDADES DIGITALES 2024",
+    "autor" => "formacion@uns",
+    "resumen" => "La universidad ofrecerá programas de certificación en habilidades digitales para preparar a los estudiantes para el futuro laboral.",
 ];
 ?>

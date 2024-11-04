47faad40115ebfd308fce459a9fda97109a72e0c
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:40:32 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -3,7 +3,7 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
-$opiniones = [
+$opinion = [
     "titulo" => "LA IMPORTANCIA DE LA EDUCACIÓN DIGITAL EN EL MUNDO MODERNO",
     "autor" => "opiniones@uns",
     "resumen" => "En un mundo cada vez más digital, la educación en habilidades tecnológicas se vuelve crucial para el desarrollo personal y profesional.",

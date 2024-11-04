e46d13963bc20aa84016592a2596b4f53c130bcf
JhenderValerio <jhendervp@gmail.com>
2024-11-04 10:32:51 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "LANZAMIENTO DE UN NUEVO SMARTPHONE CON TECNOLOGÍA DE INTELIGENCIA ARTIFICIAL",
+    "autor" => "tecnologia@uns",
+    "resumen" => "El último modelo de smartphone promete revolucionar la experiencia del usuario con capacidades avanzadas de inteligencia artificial.",
 ];
 ?>
